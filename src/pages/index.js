8c654fbfae9620bf8a121234aa5d42ba1387d5c9
Tapan Bhakhar <152768645+Tapan-Bhakhar@users.noreply.github.com>
2025-04-02 17:24:44 +0530

    Update navigation links and refine global styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
+import Link from "next/link";
 import styles from "@/styles/Home.module.css";
 
 export default function Home() {
@@ -14,35 +15,35 @@ export default function Home() {
       </Head>
       <nav className={styles.mainnav}>
         <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Blog</li>
-          <li>Contact</li>
+          <Link href="/"><li>Home</li></Link>
+          <Link href="/about"><li>About</li></Link>
+          <Link href="/blog"><li>Blog</li></Link>
+          <Link href="/contact"><li>Contact</li></Link>
         </ul>
       </nav>
-      
-        <main className={styles.main}>
-          <h1
-            className={styles.title}
-            priority
-          > Hunting Coder </h1>
-          <p className={styles.description}>
-            A blog for hunting coders
-          </p>
 
-          <div className="blogs">
-            <h2>Popular Blogs</h2>
-            <div className="blogItem">
-              <h3>how to learn javascript in 2022</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor in tenetur blanditiis porro, quasi incidunt earum quam odio deserunt aperiam assumenda quaerat.</p>
-              <h3>how to learn javascript in 2022</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor in tenetur blanditiis porro, quasi incidunt earum quam odio deserunt aperiam assumenda quaerat.</p>
-              <h3>how to learn javascript in 2022</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor in tenetur blanditiis porro, quasi incidunt earum quam odio deserunt aperiam assumenda quaerat.</p>
-            </div> 
+      <main className={styles.main}>
+        <h1
+          className={styles.title}
+          priority
+        > Hunting Coder </h1>
+        <p className={styles.description}>
+          A blog for hunting coders
+        </p>
+
+        <div className="blogs">
+          <h2>Popular Blogs</h2>
+          <div className="blogItem">
+            <h3>how to learn javascript in 2022</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor in tenetur blanditiis porro, quasi incidunt earum quam odio deserunt aperiam assumenda quaerat.</p>
+            <h3>how to learn javascript in 2022</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor in tenetur blanditiis porro, quasi incidunt earum quam odio deserunt aperiam assumenda quaerat.</p>
+            <h3>how to learn javascript in 2022</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor in tenetur blanditiis porro, quasi incidunt earum quam odio deserunt aperiam assumenda quaerat.</p>
           </div>
-        </main>
-    
+        </div>
+      </main>
+
     </div>
   );
 }
