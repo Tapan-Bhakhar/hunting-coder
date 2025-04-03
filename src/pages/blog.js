30c69b73fd644d85f94c7bdd204226f10a17399d
Tapan Bhakhar <152768645+Tapan-Bhakhar@users.noreply.github.com>
2025-04-03 12:14:13 +0530

    Implement Blog and Blog Post pages with styling and links

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from 'react';
+// import styles from '../styles/Blog.module.css';
+import styles from "@/styles/Blog.module.css";
+import Link from "next/link";
 
 const Blog = () => {
   return (
-    <div>
-      this is my blog page.
+    <main className={styles.main}>
+    <div className={styles.container}>
+      <div >
+        <Link href={'/blogpost/learn-javascript'}>
+        <h3 className={styles.blogItemh3}>how to learn javascript in 2022</h3></Link>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor in tenetur blanditiis porro, quasi incidunt earum quam odio deserunt aperiam assumenda quaerat.</p>
+        <h3 className={styles.blogItemh3}>how to learn javascript in 2022</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor in tenetur blanditiis porro, quasi incidunt earum quam odio deserunt aperiam assumenda quaerat.</p>
+        <h3 className={styles.blogItemh3}>how to learn javascript in 2022</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor in tenetur blanditiis porro, quasi incidunt earum quam odio deserunt aperiam assumenda quaerat.</p>
+      </div>
     </div>
+    </main>
   )
 }
 
--- src/pages/blogpost/[slug].js
+++ src/pages/blogpost/[slug].js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import styles from "@/styles/BlogPost.module.css";
 
 const Slug = () => {
     const router = useRouter();
     const { slug } = router.query; 
 
   return (
-    <div>
-      {slug}
+    <div className={styles.container}>
+       <main className={styles.main}>
+      <h1>Title of the page {slug} sas</h1>
+      <hr />
+      <div>
+       lorem80
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor in tenetur blanditiis porro, quasi incidunt earum quam odio deserunt aperiam assumenda quaerat. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor in tenetur blanditiis porro, quasi incidunt earum quam odio deserunt aperiam assumenda quaerat. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor in tenetur blanditiis porro, quasi incidunt earum quam odio deserunt aperiam assumenda quaerat.
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor in tenetur blanditiis porro, quasi incidunt earum quam odio deserunt aperiam assumenda quaerat. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor in tenetur blanditiis porro, quasi incidunt earum quam odio deserunt aperiam assumenda quaerat. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor in tenetur blanditiis porro, quasi incidunt earum quam odio deserunt aperiam assumenda quaerat.
+      </div>
+      </main>
     </div>
   )
 }
