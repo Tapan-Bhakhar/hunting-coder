de5811a33cc96077a233dade9487b31b325a7431
Tapan Bhakhar <152768645+Tapan-Bhakhar@users.noreply.github.com>
2025-04-04 16:07:09 +0530

    Enhance About page layout and content; add styles for improved presentation

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import styles from '@/styles/About.module.css'
 
 const About = () => {
   return (
-    <div>
-      This is my about page. 
+    
+    <div className={styles.container}>
+      <h1 className={styles.center}>About Hunting Coder</h1>
+      <h2>Introduction</h2>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor velit, aspernatur voluptatum itaque tempore saepe sed officiis expedita perferendis est vel neque omnis voluptates dolore quibusdam eius non voluptatibus facilis asperiores ducimus eveniet? Delectus, ut. Iusto earum quibusdam iste non beatae? Deserunt porro ex doloremque, nihil qui dolore aut culpa eos quam architecto modi esse unde quos quisquam et reiciendis quod velit dolorem officia voluptates blanditiis? Reiciendis fugiat modi ratione a aut, quia iste suscipit repudiandae dolorum ab magnam, optio nam repellat ad explicabo porro voluptatem quibusdam quam hic excepturi rem veniam! Nam ipsum dicta iusto magni similique, dolorem dolores in volupta</p>
+      <h2>Services Offered</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis iusto dolorem maiores illum optio necessitatibus inventore quis excepturi. Architecto nisi minus quaerat laudantium totam ex eveniet sunt, illum possimus iusto deleniti incidunt suscipit hic explicabo.</p>
+      <p>We offer the following services</p>
+      <ul>
+        <li>service 1</li>
+        <li>service 2</li>
+        <li>service 3</li>
+        <li>service 4</li>
+        <li>service 5</li>
+        <li>service 6</li>
+        <li>service 7</li>
+        <li>service 8</li>
+      </ul>
+      <h2>Contact Us</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque odit iste a ad aliquid, temporibus eaque voluptates! Voluptatem, soluta deserunt quo numquam doloremque recusandae praesentium dolor veritatis. Minus nisi quibusdam maiores non laudantium saepe qui tenetur sapiente architecto aut asperiores debitis adipisci, doloribus voluptatem esse tempore eius nostrum earum. Voluptate necessitatibus ullam eius aliquam ex odio.</p>
+
     </div>
   )
 }
